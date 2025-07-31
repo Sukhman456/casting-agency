828b1b57d4bc7ee0bda5bcef76ed6c5d16b3b89e
Sukhman <Sukhmanaulakh0472@gmail.com>
2025-07-31 08:29:14 +0530

    Fix import to lowercase app.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import AppWrapper from './app'; 
 
-function App() {
-  return <h1>Hello Casting Agency</h1>;
-}
+
 
 const rootElement = document.getElementById('root');
 console.log('rootElement:', rootElement); // Should NOT be null
 
 const root = ReactDOM.createRoot(rootElement);
-root.render(<App />);
+root.render(<AppWrapper />);
